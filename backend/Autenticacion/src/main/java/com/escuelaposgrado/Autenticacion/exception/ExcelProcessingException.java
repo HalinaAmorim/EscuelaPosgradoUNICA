2866639260855015dc0efcdc839a208e942b1fe3
HalinaAmorim <105753612+HalinaAmorim@users.noreply.github.com>
2026-08-12 14:07:08 -0300

    Mejora ExcelProcessingException con fabricas de creacion.

--- backend/Autenticacion/src/main/java/com/escuelaposgrado/Autenticacion/exception/ExcelProcessingException.java
+++ backend/Autenticacion/src/main/java/com/escuelaposgrado/Autenticacion/exception/ExcelProcessingException.java
@@ -5,11 +5,21 @@ package com.escuelaposgrado.Autenticacion.exception;
  */
 public class ExcelProcessingException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public ExcelProcessingException(String message) {
         super(message);
     }
 
     public ExcelProcessingException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public static ExcelProcessingException withMessage(String message) {
+        return new ExcelProcessingException(message);
+    }
+
+    public static ExcelProcessingException withCause(String message, Throwable cause) {
+        return new ExcelProcessingException(message, cause);
     }
 }
